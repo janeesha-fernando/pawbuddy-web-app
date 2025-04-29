144a25df125212e13126fb962383bf3372faadf9
janeesha <janeesha.dev@gmail.com>
2025-04-29 21:47:50 +0530

    feat(all): add fonts and color scheme

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,16 +1,18 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Catamaran, Noto_Sans } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const catamaran = Catamaran({
   subsets: ["latin"],
-});
+  display: "swap",
+  variable: "--font-catamaran",
+})
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const noto_sans = Noto_Sans({
   subsets: ["latin"],
-});
+  display: "swap",
+  variable: "--font-noto-sans",
+})
 
 export const metadata: Metadata = {
   title: "PawBuddy",
@@ -25,7 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${catamaran.variable} ${noto_sans.variable} antialiased`}
       >
         {children}
       </body>
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -3,9 +3,9 @@ import React from 'react'
 
 export default function page(){
   return (
-    <div className='my-20 flex flex-col items-center justify-center gap-5'>
+    <div className='my-20 flex flex-col items-center justify-center gap-5 font-catamaran'>
       <h1 className='text-3xl'>Welcome to PawBuddy</h1>
-      <Button className='p-5 bg-purple-200 text-purple-950 text-xl'>Get Started</Button>
+      <Button className='p-7 bg-purple-200 text-purple-950 text-2xl'>Get Started</Button>
     </div>
   )
 }
